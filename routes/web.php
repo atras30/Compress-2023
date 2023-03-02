89a347532219a3beba3d5840e8149477462d12fc
NabilRakhaD <nabilrakha22@gmail.com>
2023-03-03 01:00:42 +0700

    excel dan viewmore done

--- routes/web.php
+++ routes/web.php
@@ -24,15 +24,10 @@ Route::prefix('/admin')->group(function () {
     Route::post('/login', [AdminController::class, "verifyLogin"])->middleware('guest');
     Route::post('/logout', [AdminController::class, "logout"])->name('logout');
 
-<<<<<<< HEAD
     Route::get('/dashboard', [AdminController::class, "dashboard"]);
     Route::get('/dashboard/{id}', [AdminController::class, "viewmore"])->name('viewmore');
     Route::get('/export', [AdminController::class, "export"])->name('export');
     
-=======
-    Route::get('/dashboard', [AdminController::class, "dashboard"])->middleware('auth');
-    Route::get('/export', [AdminController::class, "export"])->middleware('auth')->name('export');
->>>>>>> 7cc630d28d489c4f84dbc554ec2cc69f419dd831
 });
 
 Route::get('/form', [FormController::class, "form"])->name('form');
