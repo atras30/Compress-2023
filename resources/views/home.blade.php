d0c6e3ae274944ee9a2334e1aaf476d32800890e
OtakKentung <95601654+vannessiwata@users.noreply.github.com>
2023-03-03 00:31:36 +0700

    Ganti css jd bootstrap aja

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,11 +6,11 @@
 
 @section("contents")
 <div class="container">
-  <div class="maintit">
-      <h1>commpress 2023</h1>
-  </div>
-  
-  <a href="{{ route('form') }}" class="btn btn-primary">Go To Form Registrasi</a>
-  <a href="{{ route('divisi') }}" class="btn btn-primary">Go To Divisi</a>
+  <div class="vh-100 d-flex justify-content-center align-items-center">
+    <div class="content text-center">
+        <h1>commpress 2023</h1>
+        <a href="{{ route('form') }}" class="btn btn-primary">Go To Form Registrasi</a>
+        <a href="{{ route('divisi') }}" class="btn btn-primary">Go To Divisi</a>
+    </div>
 </div>
 @endsection
