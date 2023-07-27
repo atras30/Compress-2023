563d90a95171ae74e40cbd9b99ba81aa76b6d9e2
Vallencius <vallencius.siswanto@student.umn.ac.id>
2023-07-27 10:01:18 +0700

    home desktop view

--- resources/views/newHome.blade.php
+++ resources/views/newHome.blade.php
@@ -1,6 +1,7 @@
 @extends('layout.user.master_layout')
 
 @section('styles')
+  <script src="https://cdn.tailwindcss.com"></script>
   <style>
     @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap');
 
@@ -43,7 +44,7 @@
       padding-top: 5rem;
     }
 
-    .h-2 {
+    .h-2rem {
       height: 2rem;
     }
 
@@ -150,6 +151,9 @@
 @endsection
 
 @section('contents')
+  <div class="w-full overflow-w-hidden">
+    
+  </div>
   <div class="main-content first-section position-relative yellow">
     <section class="h-100">
       <div class="mh-100">
@@ -159,8 +163,8 @@
         <img class="position-absolute overflow-hidden cloud-4" src="{{ asset('images/home/components/awan-kanan-bawah.png') }}" alt="cloud-4">
       </div>
       <img class="position-absolute start-0 w-100 h-100 sun-bg" src="{{ asset('images/home/bg/sun-bg.png') }}" alt="pattern">
-      <img class="position-absolute start-0 w-100 h-2 bar" src="{{ asset('/images/workshop/base/Top.png') }}" alt="top">
-      <img class="position-absolute bottom-0 w-100 h-2 bar" src="{{ asset('/images/workshop/base/bottom.png') }}" alt="bottom">
+      <img class="position-absolute start-0 w-100 h-2rem bar" src="{{ asset('/images/workshop/base/Top.png') }}" alt="top">
+      <img class="position-absolute bottom-0 w-100 h-2rem bar" src="{{ asset('/images/workshop/base/bottom.png') }}" alt="bottom">
     </section>
 
     <section class="position-relative ptop-5 mw-100 mx-3">
@@ -180,16 +184,86 @@
     </section>
   </div>
 
-  <div class="mh-100vh green spiral-bg position-relative d-flex justify-content-center align-items-center" style="z-index: 5;">
-    <section class="h-100">
-      <img class="position-absolute bottom-0 start-0 w-100 h-2" src="{{ asset('/images/workshop/base/bottom.png') }}" alt="bottom">
-    </section>
-    <section class="position-relative text-center ptop-5 mw-100">
-      <div class="px-5 mb-4">
-        <h1 class="fw-bold fs-4">FOTO ON PROGRESS DARI DOKUM</h1>
+  <div class="min-h-screen w-full relative">
+    <div class="flex flex-row w-full h-full z-50 absolute justify-around items-center">
+      <div class="w-1/3 h-screen">
+        <img class="h-full w-full object-fill" src="{{ asset('/images/home/bg/kotakR.svg') }}" alt="bottom">
       </div>
-    </section>
+      <div class="w-2/5 h-screen">
+        <img class="h-full w-full object-fill" src="{{ asset('/images/home/bg/kotakL.svg') }}" alt="bottom">
+      </div>
+    </div>
+    <div class="flex flex-row w-full h-full z-50 absolute justify-around items-center">
+      <div class="w-1/3 px-12">
+        <h1 class="text-center text-xl text-white drop-shadow-[0_4px_4px_rgba(0,0,0,1)]" style="font-family: Bazinga; text-shadow: -2px -2px 0 #000, 2px -2px 0 #000, -2px 2px 0 #000, 2px 2px 0 #000;">DAMPAK INTERNAL</h1>
+        <ul class="list-disc text-sm mt-4 font-bold" style="font-family: 'Inter', sans-serif;">
+          <li>Meningkatkan awareness tentang Jurnalistik kepada mahasiswa UMN,</li>
+          <li>Mengasah kemampuan dan keterampilan mahasiswa UMN terhadap kerja Jurnalistik,</li>
+          <li>Memperkenalkan kemajuan teknologi di bidang Jurnalistik kepada mahasiswa UMN,</li>
+          <li>Memperkenalkan Komik Jurnalistik dalam bentuk digital,</li>
+          <li>Menambah pengetahuan dan pengalaman mahasiswa tentang kerja jurnalistik</li>
+        </ul>
+      </div>
+      <div class="w-1/3 px-12">
+        <h1 class="text-center text-xl text-white drop-shadow-[0_4px_4px_rgba(0,0,0,1)]" style="font-family: Bazinga; text-shadow: -2px -2px 0 #000, 2px -2px 0 #000, -2px 2px 0 #000, 2px 2px 0 #000;">DAMPAK EKSTERNAL</h1>
+        <ul class="list-disc text-sm mt-4 font-bold" style="font-family: 'Inter', sans-serif;">
+          <li>Memperkenalkan UMN kepada pihak eksternal secara lebih luas,</li>
+          <li>Memperkenalkan karya jurnalistik mahasiswa/i UMN kepada pihak eksternal,</li>
+          <li>Memperkenalkan Program Studi Jurnalistik kepada pihak eksternal,</li>
+          <li>Memperkenalkan COMMPRESS UMN kepada pihak eksternal, tidak hanya siswa dan mahasiswa, tetapi juga perusahaan media jurnalisme yang ikut terlibat kerja sama,</li>
+          <li>Mendorong minat mahasiswa/i Indonesia dalam bidang jurnalistik melalui workshop dan lomba bertemakan jurnalistik.</li>
+        </ul>
+      </div>
+    </div>
+    <img class="h-full absolute bottom-0 z-40" src="{{ asset('/images/home/bg/bgsplit.png') }}" alt="bottom">
   </div>
+
+  <div class="min-h-[150vh] w-full relative">
+    <div class="flex flex-col w-full h-full z-50 absolute justify-center items-center gap-12">
+      <h1 class="text-white text-4xl drop-shadow-[0_4px_4px_rgba(0,0,0,1)]" style="font-family: Bazinga; text-shadow: -2px -2px 0 #000, 2px -2px 0 #000, -2px 2px 0 #000, 2px 2px 0 #000;">LATEST ACTIVITIES!</h1>
+      <div class="flex flex-col md:flex-row justify-center items-center gap-4">
+        <a href="https://www.instagram.com/p/Cu9LeiBMQwf/"><img src="{{ asset('/images/home/components/IG3.png') }}" class=""/></a>
+        <a href="https://www.instagram.com/p/CrDb_qbLlUI/"><img src="{{ asset('/images/home/components/IG1.png') }}" class=""/></a>
+        <a href="https://www.instagram.com/p/CssrBpZtZls/"><img src="{{ asset('/images/home/components/IG2.png') }}" class=""/></a>
+        
+      </div>
+    </div>
+    <img class="w-full absolute top-0 z-50" src="{{ asset('/images/home/components/flat-bar.png') }}" alt="top">
+    <img class="h-[150vh] w-full absolute bottom-0 z-40" src="{{ asset('/images/home/bg/bgactivity.png') }}" alt="bg">
+    <img class="w-full absolute bottom-0 z-50" src="{{ asset('/images/home/components/flat-bar.png') }}" alt="bottom">
+  </div>
+  
+  <div class="w-full min-h-[150vw] bg-[#3395a9] relative flex flex-col items-center ">
+    <div class="absolute w-1/2 h-full flex flex-col justify-around odd:items-end even:items-start z-20 px-auto">
+      <h1 class="mx-auto text-white text-4xl drop-shadow-[0_4px_4px_rgba(0,0,0,1)] text-decoration-underline" style="font-family: Bazinga; text-shadow: -2px -2px 0 #000, 2px -2px 0 #000, -2px 2px 0 #000, 2px 2px 0 #000;">TIMELINE</h1>
+      <a href="" class="even:mr-auto odd:ml-auto">
+        <img src="{{ asset('/images/home/components/tl1.png') }}" class="bg-white"/>
+      </a>
+      <a href="" class="even:mr-auto odd:ml-auto">
+        <img src="{{ asset('/images/home/components/tl2.png') }}" class="bg-white"/>
+      </a>
+      <a href="" class="even:mr-auto odd:ml-auto">
+        <img src="{{ asset('/images/home/components/tl3.png') }}" class="bg-white"/>
+      </a>
+      <a href="" class="even:mr-auto odd:ml-auto">
+        <img src="{{ asset('/images/home/components/tl4.png') }}" class="bg-white"/>
+      </a>
+      <a href="" class="even:mr-auto odd:ml-auto">
+        <img src="{{ asset('/images/home/components/tl5.png') }}" class="bg-white"/>
+      </a>
+      <a href="" class="even:mr-auto odd:ml-auto">
+        <img src="{{ asset('/images/home/components/tl6.png') }}" class="bg-white"/>
+      </a>
+      <a href="" class="even:mr-auto odd:ml-auto">
+        <img src="{{ asset('/images/home/components/tl7.png') }}" class="bg-white"/>
+      </a>
+      <a href="" class="even:mr-auto odd:ml-auto">
+        <img src="{{ asset('/images/home/components/tl8.png') }}" class="bg-white"/>
+      </a>
+    </div>
+    <img class="w-full min-h-full absolute bottom-0 z-10" src="{{ asset('/images/home/bg/Frame.png') }}"/>
+    <img class="absolute top-4 left-4 z-10" src="{{ asset('/images/home/components/petirL.png') }}"/>
+    <img class="absolute bottom-4 right-4 z-10" src="{{ asset('/images/home/components/petirR.png') }}"/>
   </div>
 @endsection
 
